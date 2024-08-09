26df4c85f33cbd41245e1a00140792cdeb65e651
Maxim Alpatski <MaximAlpatski@gmail.com>
2024-08-09 15:15:39 +0400

    Add power input

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from my_math import power
 
 print("Greetings human")
-print("This application calculates square of inputted number")
+print("This application calculates power of inputted number")
 
 x = int(input("Enter a number: "))
-p = power(x, 2)
+n = int(input("Enter power: "))
+p = power(x, n)
 print("Square of the number is: " + str(p))
