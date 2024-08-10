0c684d7ec9b61d37038d198a3e525414c9135b9c
Maxim Alpatski <MaximAlpatski@gmail.com>
2024-08-10 13:29:32 +0400

    Added print_maxim to main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from my_math import power
+from maxim import *
 
 print("Greetings human")
+print_maxim()
 print("This application calculates power of inputted number")
 
 x = int(input("Enter a number: "))
